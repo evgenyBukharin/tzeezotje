54421d6f6ea0bdd34333b8e41cdd1447b43be5e3
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-01-30 16:53:53 +0500

    form done

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -3,13 +3,17 @@ const modalContainer = document.querySelector(".modal__container");
 const modalForm = document.querySelector(".modal__form");
 const submitBtn = document.getElementById("submitBtn");
 const openModalBtn = document.getElementById("openModalBtn");
+const body = document.querySelector(".page__body");
 
 openModalBtn.addEventListener("click", () => {
 	modal.classList.add("modal-visible");
+	body.style.overflow = "hidden";
 });
 
-modal.addEventListener("click", () => {
+modal.addEventListener("click", (event) => {
 	modal.classList.remove("modal-visible");
+	body.style.overflow = "visible";
+	event.stopPropagation();
 });
 
 modalContainer.addEventListener("click", (event) => {
